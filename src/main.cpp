c495f923f39a8fd3bad267ba36ec4f5c6afb068a
ael-mouz <ael-mouz@student.1337.ma>
2023-10-25 22:40:04 +0100

    add checker function in config file and add some functions

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ael-mouz <ael-mouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 19:47:52 by ael-mouz          #+#    #+#             */
-/*   Updated: 2023/10/25 22:29:21 by ael-mouz         ###   ########.fr       */
+/*   Updated: 2023/10/25 22:39:30 by ael-mouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int ac, char **av)
         Config config;
         config.parser(av[1]);
 #ifdef DEBUG_C
-        config.printConfig();
+        config.printConfig(); 
 #endif
         StartSrever(config.getServerConfig(0));
         return 0;
